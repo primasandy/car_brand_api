61fac1a5e7ddbf3a73dc378a0e7a7d642374eccb
primasandy <40583559+primasandy@users.noreply.github.com>
2024-11-13 17:13:38 +0700

    Update config.py

--- app/config.py
+++ app/config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     # URL database, bisa disesuaikan untuk MySQL atau SQLite
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root@localhost/car_brands_db'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://dbcarbrands_whalearrow:77c96c479fc9862664c479836a24a2ade53d7ab4@oalfp.h.filess.io:3305/dbcarbrands_whalearrow'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
